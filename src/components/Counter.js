a17d46f84f8fcc0d70cc61253c8cb1305f626ff3
Wasanta <kwan_u_day@hotmail.com>
2021-11-23 20:15:30 -0500

    add

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,8 +4,13 @@ function Counter() {
   const [count, setCount] = useState(0);
 
   function increment() {
-    setCount(count + 1);
+    // setCount((count) => count + 1);
+    // setCount(count+1)
+    setCount(count => count+1)
+    
+    
   }
+  console.log(count)
 
   return <button onClick={increment}>I have been clicked {count} times</button>;
 }
